af167bbfce3b61cb9b1ca6dd7cab4e0594393de4
box-sdk-build <sdk-build@box.com>
2023-03-14 03:00:49 -0700

    generated with codegen at box/box-codegen@c0fb2fe and spec at box/box-openapi@ba9012e

--- lib/managers/avatars.generated.d.ts
+++
@@ -1,9 +0,0 @@
-import { DeveloperTokenAuth } from "../developerTokenAuth.js";
-import { CCGAuth } from "../ccgAuth.js";
-export type AvatarsManagerAuthField = DeveloperTokenAuth | CCGAuth;
-export declare class AvatarsManager {
-    readonly auth: AvatarsManagerAuthField;
-    constructor(fields: Omit<AvatarsManager, "getUsersIdAvatar" | "deleteUsersIdAvatar">);
-    getUsersIdAvatar(userId: string): Promise<any>;
-    deleteUsersIdAvatar(userId: string): Promise<any>;
-}
