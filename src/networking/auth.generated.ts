6d0984524d82765da925bc2081eadba5a5d03c66
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2024-04-26 13:16:04 +0200

    feat: Support revoking and downscoping token for BoxDeveloperTokenAuth (#147)

--- src/networking/auth.generated.ts
+++ src/networking/auth.generated.ts
@@ -4,4 +4,11 @@ export interface Authentication {
   retrieveToken(networkSession?: NetworkSession): Promise<AccessToken>;
   refreshToken(networkSession?: NetworkSession): Promise<AccessToken>;
   retrieveAuthorizationHeader(networkSession?: NetworkSession): Promise<string>;
+  revokeToken(networkSession?: NetworkSession): Promise<undefined>;
+  downscopeToken(
+    scopes: readonly string[],
+    resource?: string,
+    sharedLink?: string,
+    networkSession?: NetworkSession
+  ): Promise<AccessToken>;
 }
