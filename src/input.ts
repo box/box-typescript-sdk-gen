d6aa01262a1b0eec9b88b86659449f8db1ec657d
box-sdk-build <sdk-build@box.com>
2024-04-26 07:26:38 -0700

    feat: remove insensitive language (box/box-codegen#477)

--- src/input.ts
+++ src/input.ts
@@ -1,5 +1,5 @@
 export async function input(message: string, type: unknown) {
   // TODO: can use https://www.npmjs.com/package/prompts for Node
-  await new Promise((resolve) => setTimeout(resolve, 1000)); // dummy delay
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // placeholder delay
   return void 0;
 }
