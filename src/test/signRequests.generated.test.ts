c04954ae6d639af1fb167acb999a85d1352a6740
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2024-06-04 12:59:43 +0200

    test: Update sign request integration tests (box/box-codegen#499) (#214)

--- src/test/signRequests.generated.test.ts
+++ src/test/signRequests.generated.test.ts
@@ -8,6 +8,8 @@ import { serializeSignRequestCreateRequest } from '../schemas/signRequestCreateR
 import { deserializeSignRequestCreateRequest } from '../schemas/signRequestCreateRequest.generated.js';
 import { serializeSignRequestCreateSigner } from '../schemas/signRequestCreateSigner.generated.js';
 import { deserializeSignRequestCreateSigner } from '../schemas/signRequestCreateSigner.generated.js';
+import { serializeSignRequestCreateSignerRoleField } from '../schemas/signRequestCreateSigner.generated.js';
+import { deserializeSignRequestCreateSignerRoleField } from '../schemas/signRequestCreateSigner.generated.js';
 import { serializeFolderMini } from '../schemas/folderMini.generated.js';
 import { deserializeFolderMini } from '../schemas/folderMini.generated.js';
 import { serializeSignRequestPrefillTag } from '../schemas/signRequestPrefillTag.generated.js';
@@ -24,6 +26,7 @@ import { FolderFull } from '../schemas/folderFull.generated.js';
 import { SignRequest } from '../schemas/signRequest.generated.js';
 import { SignRequestCreateRequest } from '../schemas/signRequestCreateRequest.generated.js';
 import { SignRequestCreateSigner } from '../schemas/signRequestCreateSigner.generated.js';
+import { SignRequestCreateSignerRoleField } from '../schemas/signRequestCreateSigner.generated.js';
 import { FolderMini } from '../schemas/folderMini.generated.js';
 import { SignRequestPrefillTag } from '../schemas/signRequestPrefillTag.generated.js';
 import { FileBase } from '../schemas/fileBase.generated.js';
@@ -35,9 +38,9 @@ import { dateToString } from '../internal/utils.js';
 import { uploadNewFile } from './commons.generated.js';
 import { createNewFolder } from './commons.generated.js';
 import { getDefaultClient } from './commons.generated.js';
-import { Date } from '../internal/utils.js';
 import { toString } from '../internal/utils.js';
 import { sdToJson } from '../serialization/json.js';
+import { Date } from '../internal/utils.js';
 import { SerializedData } from '../serialization/json.js';
 import { sdIsEmpty } from '../serialization/json.js';
 import { sdIsBoolean } from '../serialization/json.js';
@@ -52,8 +55,31 @@ test('testCreateGetCancelAndListSignRequest', async function testCreateGetCancel
   const destinationFolder: FolderFull = await createNewFolder();
   const createdSignRequest: SignRequest =
     await client.signRequests.createSignRequest({
-      signers: [{ email: signerEmail } satisfies SignRequestCreateSigner],
+      signers: [
+        {
+          email: signerEmail,
+          suppressNotifications: true,
+          declinedRedirectUrl: 'https://www.box.com',
+          embedUrlExternalUserId: '123',
+          isInPerson: false,
+          loginRequired: false,
+          password: 'password',
+          role: 'signer' as SignRequestCreateSignerRoleField,
+        } satisfies SignRequestCreateSigner,
+      ],
+      areRemindersEnabled: true,
+      areTextSignaturesEnabled: true,
+      daysValid: 30,
+      declinedRedirectUrl: 'https://www.box.com',
+      emailMessage: 'Please sign this document',
+      emailSubject: 'Sign this document',
+      externalId: '123',
+      externalSystemName: 'BoxSignIntegration',
+      isDocumentPreparationNeeded: false,
+      isPhoneVerificationRequiredToView: false,
+      name: 'Sign Request',
       parentFolder: new FolderMini({ id: destinationFolder.id }),
+      redirectUrl: 'https://www.box.com',
       prefillTags: [
         {
           dateValue: dateFromString('2035-01-01'),
@@ -62,10 +88,71 @@ test('testCreateGetCancelAndListSignRequest', async function testCreateGetCancel
       ],
       sourceFiles: [new FileBase({ id: fileToSign.id })],
     } satisfies SignRequestCreateRequest);
+  if (!(createdSignRequest.areRemindersEnabled == true)) {
+    throw new Error('Assertion failed');
+  }
+  if (!(createdSignRequest.areTextSignaturesEnabled == true)) {
+    throw new Error('Assertion failed');
+  }
+  if (!(createdSignRequest.daysValid == 30)) {
+    throw new Error('Assertion failed');
+  }
+  if (!(createdSignRequest.declinedRedirectUrl == 'https://www.box.com')) {
+    throw new Error('Assertion failed');
+  }
+  if (!(createdSignRequest.emailMessage == 'Please sign this document')) {
+    throw new Error('Assertion failed');
+  }
+  if (!(createdSignRequest.emailSubject == 'Sign this document')) {
+    throw new Error('Assertion failed');
+  }
+  if (!(createdSignRequest.externalId == '123')) {
+    throw new Error('Assertion failed');
+  }
+  if (!(createdSignRequest.externalSystemName == 'BoxSignIntegration')) {
+    throw new Error('Assertion failed');
+  }
+  if (!(createdSignRequest.isDocumentPreparationNeeded == false)) {
+    throw new Error('Assertion failed');
+  }
+  if (!(createdSignRequest.isPhoneVerificationRequiredToView == false)) {
+    throw new Error('Assertion failed');
+  }
+  if (!(createdSignRequest.name == 'Sign Request')) {
+    throw new Error('Assertion failed');
+  }
+  if (!(createdSignRequest.redirectUrl == 'https://www.box.com')) {
+    throw new Error('Assertion failed');
+  }
   if (!(createdSignRequest.signFiles!.files![0].name == fileToSign.name)) {
     throw new Error('Assertion failed');
   }
   if (!(createdSignRequest.signers![1].email == signerEmail)) {
+    throw new Error('Assertion failed');
+  }
+  if (!(createdSignRequest.signers![1].suppressNotifications == true)) {
+    throw new Error('Assertion failed');
+  }
+  if (
+    !(
+      createdSignRequest.signers![1].declinedRedirectUrl ==
+      'https://www.box.com'
+    )
+  ) {
+    throw new Error('Assertion failed');
+  }
+  if (!(createdSignRequest.signers![1].embedUrlExternalUserId == '123')) {
+    throw new Error('Assertion failed');
+  }
+  if (!(createdSignRequest.signers![1].isInPerson == false)) {
+    throw new Error('Assertion failed');
+  }
+  if (!(createdSignRequest.signers![1].loginRequired == false)) {
+    throw new Error('Assertion failed');
+  }
+  if (
+    !((toString(createdSignRequest.signers![1].role) as string) == 'signer')
+  ) {
     throw new Error('Assertion failed');
   }
   if (!(createdSignRequest.parentFolder!.id == destinationFolder.id)) {
