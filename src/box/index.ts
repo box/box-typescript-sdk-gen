9b502eff91b974ff0e62040ead4fc506dd6e7675
box-sdk-build <sdk-build@box.com>
2025-08-26 04:12:31 -0700

    feat: Adjust import and export in TS (box/box-codegen#801)

--- src/box/index.ts
+++ src/box/index.ts
@@ -1,4 +1,8 @@
-export { BoxCcgAuth, CcgConfig } from './ccgAuth.generated';
-export { BoxDeveloperTokenAuth } from './developerTokenAuth.generated';
-export { BoxJwtAuth, JwtConfig } from './jwtAuth.generated';
-export { BoxOAuth, OAuthConfig } from './oauth.generated';
+export * from './index';
+export * from './eventStream';
+export * from './errors';
+export * from './tokenStorage.generated';
+export * from './developerTokenAuth.generated';
+export * from './oauth.generated';
+export * from './jwtAuth.generated';
+export * from './ccgAuth.generated';
